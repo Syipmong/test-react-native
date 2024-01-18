adbe9fe776ac852c73c149f2c62b379dfece83f3
syipmong <syipmong5@gmail.com>
2024-01-18 22:25:40 +0100

    initial push

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, SafeAreaView } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import NavBar from './app/components/NavBar';
+
+export default function App() {
+    return (
+        <SafeAreaView>
+        <NavBar/>
+       </SafeAreaView> 
+        
+    );
+}
---
+++ app/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View , Text } from "react-native";
+
+const NavBar = () => {
+    return (
+        <View>
+            <Text>NavBar</Text>
+        </View>
+    );
+}
+
+export default NavBar;
